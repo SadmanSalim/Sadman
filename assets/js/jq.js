aab9ff003ad64daa00583012264d617399b0e068
SadmanSalim <sadmanmd225@gmail.com>
2024-11-27 16:44:45 +0600

    1

--- assets/js/jq.js
+++ assets/js/jq.js
@@ -1,9 +1,9 @@
 $(window).scroll(function () {
     var scroll = $(window).scrollTop();
-    if (scroll >= 300) {
+    if (scroll >= 200) {
         $('nav').addClass('fixed');
     }
-    if(scroll <= 300){
+    if(scroll <= 200){
         $('nav').addClass('fixedRemoved');
     }else{
         $('nav').removeClass('fixedRemoved');
